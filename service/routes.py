3175b50760f75d3f80ac27bc8268f568c77f2b98
ma-ngg <elizabeth.a.kuznetsova@gmail.com>
2024-11-24 11:23:36 -0500

    Added badge for GitHub Actions

--- service/routes.py
+++ service/routes.py
@@ -61,6 +61,7 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -93,6 +94,7 @@ def get_accounts(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
